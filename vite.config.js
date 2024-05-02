f2af3bbd9212322d5f5eaa53fce5a97380791b8b
chocoscoding <chocoscoding@gmail.com>
2024-05-02 09:20:00 -0500

    init

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+	// eslint-disable-next-line no-undef
+	const env = loadEnv(mode, process.cwd());
+
+	console.log(env.VITE_API_URL);
+
+	const API_URL = `${env.VITE_API_URL}`;
+
+	return {
+		plugins: [react()],
+		server: {
+			port: 3000,
+			proxy: {
+				"/api": {
+					target: API_URL,
+					changeOrigin: true,
+				},
+			},
+		},
+	};
+});
